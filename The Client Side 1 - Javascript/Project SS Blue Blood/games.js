b433c43835a15a9de2d64bc1b9b583b6aba45038
DavidPerry06 <dperry@bayviewglen.ca>
2022-11-17 14:21:44 -0500

    update

--- The Client Side 1 - Javascript/Project SS Blue Blood/games.js
+++ The Client Side 1 - Javascript/Project SS Blue Blood/games.js
@@ -1,10 +1,32 @@
-function start() {
    const dataString = localStorage.getItem('teams');
    const teams = JSON.parse(dataString);
    let params = (new URL(document.location)).searchParams;
-
    const team = teams.filter(team => team.Id == params.get('id'));
 
-   document.querySelector('#test').textContent = team[0].Name;
+   displayTeam()
 
-   }
+function displayTeam(){
+   document.querySelector('#test').textContent = team[0].Name;
+}
+
+displayGames();
+
+function displayGames(){
+   let section = document.querySelector('#container');
+   team[0].games.foreach((gameData)=>{
+      let div = document.createElement('div')
+      div.setAttribute('class', 'card');
+      let divContent = document.createElement('div');
+      divContent.setAttribute('class', 'card-content')
+      let divHeader = document.createElement('header');
+      divHeader.setAttribute('class', 'card-header-title')
+      let oppArr = teams.filter(team => team.id == gameData.opp)
+      let opp = oppArr[0].name;
+      divContent.textContent = opp;
+      divHeader.textContent = "Date: " + gameData.day;
+
+      section.appendChild(div)
+      div.appendChild(divHeader)
+      div.appendChild(divContent)
+   })
+}
